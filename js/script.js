ab399b07e3ff5a25b508fbd82d4805ec23eaff68
Gustavo Murilo <gustavo.barbosa@dcomp.ufs.br>
2021-06-14 18:42:10 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("button");
+const img = document.getElementById("img");
+
+button.addEventListener('click', function(){
+    img.style.display = "inline";
+});
